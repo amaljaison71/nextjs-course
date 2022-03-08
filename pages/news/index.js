e20df18d31bb7a22950234b7765aebb1277ef7db
Amal Jaison <amaljaison71@gmail.com>
2022-03-08 11:07:43 +0530

    Linking between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,18 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>;
+      <ul>
+        <Link href="/news/nextjs-is-a-great-framework">
+          <li>NextJs is A Great Framework</li>
+        </Link>
+        <li>Something else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
